688364af40c56600ccad1cd5bdd93dd138594d55
Chirag-creator17 <chiraggoel172002@gmail.com>
2023-09-09 10:26:29 +0530

    added footer

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import { LandingPageComponent } from "../features/Home/Landing";
 import { Navbar } from "../features/navbar/Navbar";
-
+import { FooterComponent } from "../features/footer/footer";
 function HomePage() {
   return (
     <>
       <Navbar></Navbar>
       <LandingPageComponent />
+      <FooterComponent />
     </>
   );
 }
