83d9370646610ff7b6dc2f52a8199ca87bd8d90c
Vani Sharma <vanisharma826@gmail.com>
2023-09-25 20:46:59 +0530

    lawyer directory setup

--- client/src/features/LawyersList/LawyersDirectory.js
+++ client/src/features/LawyersList/LawyersDirectory.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Navbar } from "../navbar/Navbar";
+import verifiedIcon from "../../img/verifiedicon.png";
 const people = [
   {
     name: "Leslie Alexander",
@@ -10,6 +11,8 @@ const people = [
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
     lastSeenDateTime: "2023-01-23T13:23Z",
+    description:
+      "Seasoned negotiator, mediator, and attorney providing premier legal advice, services, and representation with backgrounds in the following but not limited to law areas: business/commercial (restaurant & manufacturing), contracts, education, employment,family and matrimonial, healthcare, real estate, and probate & wills/trusts",
   },
   {
     name: "Michael Foster",
@@ -19,6 +22,8 @@ const people = [
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
     lastSeenDateTime: "2023-01-23T13:23Z",
+    description:
+      "Seasoned negotiator, mediator, and attorney providing premier legal advice, services, and representation with backgrounds in the following but not limited to law areas: business/commercial (restaurant & manufacturing), contracts, education, employment,family and matrimonial, healthcare, real estate, and probate & wills/trusts",
   },
   {
     name: "Dries Vincent",
@@ -27,6 +32,8 @@ const people = [
     imageUrl:
       "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: null,
+    description:
+      "Seasoned negotiator, mediator, and attorney providing premier legal advice, services, and representation with backgrounds in the following but not limited to law areas: business/commercial (restaurant & manufacturing), contracts, education, employment,family and matrimonial, healthcare, real estate, and probate & wills/trusts",
   },
   {
     name: "Lindsay Walton",
@@ -36,6 +43,8 @@ const people = [
       "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
     lastSeenDateTime: "2023-01-23T13:23Z",
+    description:
+      "Seasoned negotiator, mediator, and attorney providing premier legal advice, services, and representation with backgrounds in the following but not limited to law areas: business/commercial (restaurant & manufacturing), contracts, education, employment,family and matrimonial, healthcare, real estate, and probate & wills/trusts",
   },
   {
     name: "Courtney Henry",
@@ -45,6 +54,8 @@ const people = [
       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
     lastSeenDateTime: "2023-01-23T13:23Z",
+    description:
+      "Seasoned negotiator, mediator, and attorney providing premier legal advice, services, and representation with backgrounds in the following but not limited to law areas: business/commercial (restaurant & manufacturing), contracts, education, employment,family and matrimonial, healthcare, real estate, and probate & wills/trusts",
   },
   {
     name: "Tom Cook",
@@ -53,6 +64,8 @@ const people = [
     imageUrl:
       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: null,
+    description:
+      "Seasoned negotiator, mediator, and attorney providing premier legal advice, services, and representation with backgrounds in the following but not limited to law areas: business/commercial (restaurant & manufacturing), contracts, education, employment,family and matrimonial, healthcare, real estate, and probate & wills/trusts",
   },
 ];
 
@@ -130,45 +143,106 @@ export function LawyerDirectory() {
             </div>
           </button>
         </div>
-        <ul role="list" className="divide-y divide-gray-100">
-          {people.map((person) => (
-            <li key={person.email} className="flex justify-around gap-x-6 py-5">
-              <div className="flex min-w-0 gap-x-4">
-                <img
-                  className="h-24 w-24 flex-none rounded-full bg-gray-50"
-                  src={person.imageUrl}
-                  alt=""
-                />
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    {person.name}
-                  </p>
-                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                    {person.email}
-                  </p>
-                </div>
-              </div>
-              <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-                {person.lastSeen ? (
-                  <p className="mt-1 text-xs leading-5 text-gray-500">
-                    Last seen{" "}
-                    <time dateTime={person.lastSeenDateTime}>
-                      {person.lastSeen}
-                    </time>
-                  </p>
-                ) : (
-                  <div className="mt-1 flex items-center gap-x-1.5">
-                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                      <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+        <div>
+          <ul role="list" className="my-15">
+            {people.map((person) => (
+              <li key={person.email} className="py-10">
+                <div className="flex justify-around py-3">
+                  <div className="flex min-w-0 gap-x-4">
+                    <img
+                      className="h-28 w-28 flex-none rounded-full bg-gray-50"
+                      src={person.imageUrl}
+                      alt=""
+                    />
+                    <div className="min-w-0 flex-auto">
+                      <p className="text-2xl mx-5 font-semibold leading-6 text-gray-900">
+                        {person.name}
+                      </p>
+                      <div className=" mx-5">
+                        <div class="flex items-center space-x-1 mt-3">
+                          <svg
+                            class="w-4 h-4 text-yellow-300"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 22 20"
+                          >
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                          </svg>
+                          <svg
+                            class="w-4 h-4 text-yellow-300"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 22 20"
+                          >
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                          </svg>
+                          <svg
+                            class="w-4 h-4 text-yellow-300"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 22 20"
+                          >
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                          </svg>
+                          <svg
+                            class="w-4 h-4 text-yellow-300"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 22 20"
+                          >
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                          </svg>
+                          <svg
+                            class="w-4 h-4 text-gray-300 dark:text-gray-500"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 22 20"
+                          >
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                          </svg>
+                        </div>
+                        <p className="mt-5 truncate leading-5 text-gray-500">
+                          <span>
+                            <img src={verifiedIcon}></img>
+                          </span>
+                          <span> Contracts Counsel verified</span>
+                        </p>
+                        <p className="mt-5 truncate leading-5 text-gray-500">
+                          Des Moines, IA
+                        </p>
+                        <p className="mt-5 truncate leading-5 text-gray-500">
+                          Massachusetts, New Hampshire
+                        </p>
+                      </div>
                     </div>
-                    <p className="text-xs leading-5 text-gray-500">Online</p>
                   </div>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
+                  <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
+                    <button className="border-[#3D00B7] font-semibold text-[#3D00B7] text-md border-2 rounded-[60px] lg:mr-8 px-6 py-0.5 lg:py-2">
+                      View Profile
+                    </button>
+                    <button className=" bg-[#3D00B7] text-white rounded-[60px] lr:mr-5 mr-2 px-6 py-0.5 lg:py-2">
+                      Schedule a call
+                    </button>
+
+                    <p className="mt-6 leading-5 text-gray-500">
+                      15 Years practicing
+                    </p>
+
+                    <div className="mt-5 flex items-center gap-x-1.5">
+                      <p className=" leading-5 text-gray-500">$225.00 / hour</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mx-56 my-5">{person.description}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
