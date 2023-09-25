81da6a0206ebf39cf3e20847dbce9c582dfe156b
Vani Sharma <vanisharma826@gmail.com>
2023-09-25 15:33:12 +0530

    search icon done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import { ChatComponent } from "./features/chat/ChatComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LawyerDirectory } from "./features/LawyersList/LawyersDirectory";
 export default function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatComponent />} />
+          <Route path="/lawyers" element={<LawyerDirectory />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ client/src/features/LawyersList/LawyersDirectory.js
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Navbar } from "../navbar/Navbar";
+const people = [
+  {
+    name: "Leslie Alexander",
+    email: "leslie.alexander@example.com",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    lastSeen: "3h ago",
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+  {
+    name: "Michael Foster",
+    email: "michael.foster@example.com",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    lastSeen: "3h ago",
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+  {
+    name: "Dries Vincent",
+    email: "dries.vincent@example.com",
+    role: "Business Relations",
+    imageUrl:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    lastSeen: null,
+  },
+  {
+    name: "Lindsay Walton",
+    email: "lindsay.walton@example.com",
+    role: "Front-end Developer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    lastSeen: "3h ago",
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+  {
+    name: "Courtney Henry",
+    email: "courtney.henry@example.com",
+    role: "Designer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    lastSeen: "3h ago",
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+  {
+    name: "Tom Cook",
+    email: "tom.cook@example.com",
+    role: "Director of Product",
+    imageUrl:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    lastSeen: null,
+  },
+];
+
+export function LawyerDirectory() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex-auto mt-8 justify-content">
+        <div className="flex m-12 justify-center">
+          <div className="flex bg-[#F6F7FA]">
+            <div class="grid place-items-center h-full w-12 text-gray-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+            <input
+              className="bg-[#F6F7FA] p-3 pr-44 rounded focus:outline-none"
+              placeholder="Practice area, legal issue"
+              type="text"
+              name="search"
+            />
+          </div>
+          <div className="flex bg-[#F6F7FA]">
+            <div class="grid border-l-2 place-items-center h-full w-12 text-gray-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+            <input
+              className="bg-[#F6F7FA] p-3 pr-44 border-solid rounded focus:outline-none"
+              placeholder="State, city"
+              type="text"
+              name="search"
+            />
+          </div>
+
+          <button className="text-white rounded">
+            <div class="grid rounded bg-[#3D00B7] place-items-center h-full w-12 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+          </button>
+        </div>
+        <ul role="list" className="divide-y divide-gray-100">
+          {people.map((person) => (
+            <li key={person.email} className="flex justify-around gap-x-6 py-5">
+              <div className="flex min-w-0 gap-x-4">
+                <img
+                  className="h-24 w-24 flex-none rounded-full bg-gray-50"
+                  src={person.imageUrl}
+                  alt=""
+                />
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">
+                    {person.name}
+                  </p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {person.email}
+                  </p>
+                </div>
+              </div>
+              <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+                {person.lastSeen ? (
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    Last seen{" "}
+                    <time dateTime={person.lastSeenDateTime}>
+                      {person.lastSeen}
+                    </time>
+                  </p>
+                ) : (
+                  <div className="mt-1 flex items-center gap-x-1.5">
+                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                      <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                    </div>
+                    <p className="text-xs leading-5 text-gray-500">Online</p>
+                  </div>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
